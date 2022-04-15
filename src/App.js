f1b0eb899d737f226bc966575b61e1f51eb50631
MyoMyintAung <myomyintaung064@gmail.com>
2022-04-15 22:47:26 +0630

    added first and last name

--- src/App.js
+++ src/App.js
@@ -12,7 +12,21 @@ const SignupForm = () => {
   return (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
-      <input />
+      <input
+        id="firstName"
+        type="text"
+        name="firstName"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -21,7 +35,9 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
-      <button type="submit">Submit</button>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
     </form>
   );
 };
